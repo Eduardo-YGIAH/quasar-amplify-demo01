a429daf1862769a49d468fb6f2a9fe12de7aded1
Eduardo Neto <edu.neto@icloud.com>
2020-10-18 11:53:02 +0100

    app pages and logic

---
+++ src/pages/Profile.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Welcome, {{ user.username }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "Profile",
+  components: {},
+  data() {
+    let userPlaceholder = {
+      username: "bar"
+    };
+    return {
+      user: userPlaceholder,
+      signedIn: false
+    };
+  },
+  beforeCreate() {
+    this.$Auth
+      .currentAuthenticatedUser()
+      .then(user => {
+        console.log("User: ", user);
+        this.user = user;
+        this.signedIn = true;
+      })
+      .catch(() => console.log("not signed in..."));
+  }
+};
+</script>
+<style></style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,52 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      {
+        path: "",
+        name: "todo",
+        component: () => import("pages/Todo.vue"),
+        meta: {
+          requiresAuth: true
+        }
+      }
     ]
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
-    path: '*',
-    component: () => import('pages/Error404.vue')
+    path: "/auth",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      {
+        path: "",
+        name: "auth",
+        component: () => import("pages/Auth.vue"),
+        meta: {
+          requiresAuth: false
+        }
+      }
+    ]
+  },
+  {
+    path: "/profile",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      {
+        name: "profile",
+        path: "",
+        component: () => import("pages/Profile.vue"),
+        meta: {
+          requiresAuth: true
+        }
+      }
+    ]
   }
-]
-
-export default routes
+];
+// Always leave this as last one
+if (process.env.MODE !== "ssr") {
+  routes.push({
+    path: "*",
+    component: () => import("pages/Error404.vue")
+  });
+}
+export default routes;
